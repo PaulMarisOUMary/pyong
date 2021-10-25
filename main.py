b54eeef6e1ff52ad9eb5806df3fc1b80d0464a4d
Paul Maris <71769515+PaulMarisOUMary@users.noreply.github.com>
2021-10-25 23:17:14 +0200

    upload working sample pong game

---
+++ main.py
@@ -0,0 +1,88 @@
+import pygame
+from pygame import locals
+
+# Window Settings
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+FPS = 60
+SPEED = 1
+Running = True
+
+# Pads Settings
+PAD_WIDTH = 20
+PAD_HEIGHT = 100
+PAD_LEFT = [PAD_WIDTH, 0]
+PAD_RIGHT = [WIN_WIDTH-PAD_WIDTH*2, 0]
+
+# Ball Settings
+BALL_RADIUS = 5
+BALL = [WIN_WIDTH/2 - BALL_RADIUS/2, WIN_HEIGHT/2 - BALL_RADIUS/2]
+BALL_vX = -3
+BALL_vY = 1
+
+# Colors
+red = (255,0,0)
+blue = (0,0,255)
+green = (0,255,0)
+
+points = 0
+
+screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+clock = pygame.time.Clock()
+
+def keys():
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEMOTION: #easier than with keys
+            if WIN_HEIGHT - PAD_HEIGHT >= event.pos[1]:
+                PAD_LEFT[1] = event.pos[1]
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                global Running
+                Running = False
+
+def moveBall():
+    global BALL, BALL_vX, BALL_vY, points, SPEED
+
+    if BALL[0] <= 0 or BALL[0] >= WIN_WIDTH: #Left & Right bounce (without pad)
+        BALL = [WIN_WIDTH/2 - BALL_RADIUS/2, WIN_HEIGHT/2 - BALL_RADIUS/2]
+        points -= 1
+        SPEED = 1
+    if BALL[1] <= 0+BALL_RADIUS or BALL[1] >= WIN_HEIGHT-BALL_RADIUS: #Top & Bottom bounce 
+        BALL_vY = -BALL_vY
+
+    if BALL[0] <= PAD_WIDTH*2+BALL_RADIUS: #Left bounce 
+        if BALL[1] >= PAD_LEFT[1] and BALL[1] <= PAD_LEFT[1]+PAD_HEIGHT: #Left & Right bounce (with pad)
+            BALL_vX = -BALL_vX
+    elif BALL[0] >= WIN_WIDTH-PAD_WIDTH*2-10: #Right bounce
+        if BALL[1] >= PAD_RIGHT[1] and BALL[1] <= PAD_RIGHT[1]+PAD_HEIGHT: #Left & Right bounce (with pad)
+            BALL_vX = -BALL_vX
+
+    BALL[0] -= BALL_vX * SPEED
+    BALL[1] -= BALL_vY * SPEED
+
+    PAD_RIGHT[1] = BALL[1]-PAD_HEIGHT/2
+
+def drawPADS():
+    pygame.draw.rect(screen, color=blue, rect=(PAD_LEFT[0], PAD_LEFT[1], PAD_WIDTH, PAD_HEIGHT), border_radius=5)
+    pygame.draw.rect(screen, color=red, rect=(PAD_RIGHT[0], PAD_RIGHT[1], PAD_WIDTH, PAD_HEIGHT), border_radius=5)
+
+def drawBALL():
+    pygame.draw.circle(screen, green, BALL, BALL_RADIUS)
+
+while Running:
+    pygame.display.set_caption("PONG : {}pts | {} FPS".format(points, int(clock.get_fps())))
+
+    keys()
+    moveBall()
+
+    # Graphical part
+    screen.fill((0, 0, 0)) # Reset Frame
+    drawPADS()
+    drawBALL()
+
+    SPEED += 0.005
+
+    # Update Rendering
+    clock.tick(FPS)
+    pygame.display.flip()
